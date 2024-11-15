45e222326e150377ee5a7dcd806c058735be2651
tsepanx <77626319+tsepanx@users.noreply.github.com>
2024-11-15 19:31:06 +0300

    new?

--- {{ cookiecutter.project_name }}/src/main.cpp
+++ {{ cookiecutter.project_name }}/src/main.cpp
@@ -1,6 +1,25 @@
+{% if cookiecutter.type == "regular" or cookiecutter.type == "boost" %}
 #include<iostream>
 
 int main() {
     std::cout << "Hello world!\n";
     return 0;
 }
+
+{% elif cookiecutter.type == "qt-gui" %}
+
+#include "mainwindow.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
+
+{% elif cookiecutter.type == "qt-cli" %}
+{% endif %}
+
